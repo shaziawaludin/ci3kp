cb1271a63fc26a1b93a7cecb20d8f1852442c360
Shazi Awaludin <shaziawaludin23@gmail.com>
2021-02-14 21:53:57 +0700

    Flash Message

--- application/views/post/tambah.php
+++ application/views/post/tambah.php
@@ -10,10 +10,12 @@
                         <div class="form-group">
                             <label for="judul">Judul</label>
                             <input type="text" class="form-control" name="judul" id="judul" placeholder="Masukkan Judul" required>
+                            <?= form_error('judul', '<small class="text-danger">', '</small>') ?>
                         </div>
                         <div class="form-group">
                             <label for="isi">Isi </label>
                             <textarea class="form-control" name="isi" id="isi" placeholder="Masukkan Isi" rows="3" required></textarea>
+                            <?= form_error('isi', '<small class="text-danger">', '</small>') ?>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
